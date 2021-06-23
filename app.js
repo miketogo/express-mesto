171957a9166a3aafc320dd2a59aaeefd308a5cca
Mikhail Surikov <miketogo@yandex.ru>
2021-06-23 23:53:25 +0300

    fix: cros test8

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-// import * as helmet from 'helmet';
+import * as helmet from 'helmet';
 
 const express = require('express');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 const { errors, celebrate, Joi } = require('celebrate');
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
@@ -13,24 +13,25 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
 
-// const CORS_WHITELIST = [
-//   'https://surikov.mesto.students.nomoredomains.monster',
-//   'https://api.surikovmesto.students.nomoredomains.club',
-//   'http://surikov.mesto.students.nomoredomains.monster',
-//   'http://api.surikovmesto.students.nomoredomains.club',
-//   'http://localhost:3000',
-// ];
+const CORS_WHITELIST = [
+  'https://surikov.mesto.students.nomoredomains.monster',
+  'https://api.surikovmesto.students.nomoredomains.club',
+  'http://surikov.mesto.students.nomoredomains.monster',
+  'http://api.surikovmesto.students.nomoredomains.club',
+  'http://localhost:3000',
+  'https://localhost:3000',
+];
 
-// const corsOption = {
-//   credentials: true,
-//   origin: function checkCorsList(origin, callback) {
-//     if (CORS_WHITELIST.indexOf(origin) !== -1 || !origin) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-// };
+const corsOption = {
+  credentials: true,
+  origin: function checkCorsList(origin, callback) {
+    if (CORS_WHITELIST.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
 const app = express();
 mongoose.connect('mongodb://localhost:27017/mestodb', {
@@ -40,15 +41,10 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useFindAndModify: false,
 });
 
+app.use(helmet());
 app.use(express.json());
 app.use(requestLogger);
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-
-//   next();
-// });
+app.use(cors(corsOption));
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required(),
